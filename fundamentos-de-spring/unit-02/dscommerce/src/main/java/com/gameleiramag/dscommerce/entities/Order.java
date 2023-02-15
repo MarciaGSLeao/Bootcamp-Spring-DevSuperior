0559126a95913f6ba8c364517e7a14a2e7656990
Marcia Gameleira <gameleira.mag@hotmail.com>
2023-02-15 17:37:18 -0300

    .

--- fundamentos-de-spring/unit-02/dscommerce/src/main/java/com/gameleiramag/dscommerce/entities/Order.java
+++ fundamentos-de-spring/unit-02/dscommerce/src/main/java/com/gameleiramag/dscommerce/entities/Order.java
@@ -2,6 +2,7 @@ package com.gameleiramag.dscommerce.entities;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import com.gameleiramag.dscommerce.enums.OrderStatus;
@@ -85,6 +86,14 @@ public class Order {
 
 	public void setPayment(Payment payment) {
 		this.payment = payment;
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	public List<Product> getProducts(){
+		return items.stream().map(x -> x.getProduct()).toList();
 	}
 	
 }
--- fundamentos-de-spring/unit-02/dscommerce/src/main/java/com/gameleiramag/dscommerce/entities/Product.java
+++ fundamentos-de-spring/unit-02/dscommerce/src/main/java/com/gameleiramag/dscommerce/entities/Product.java
@@ -1,6 +1,7 @@
 package com.gameleiramag.dscommerce.entities;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -92,4 +93,7 @@ public class Product {
 		return items;
 	}
 		
+	public List<Order> getOrders(){
+		return items.stream().map(x -> x.getOrder()).toList();
+	}
 }
